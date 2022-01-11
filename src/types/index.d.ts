81292f6d67298dd1c717a7b234b71447bfcc78ef
Felipe Lara <68015575+larafe1@users.noreply.github.com>
2022-01-11 19:05:29 -0300

    Chore: Player screen features

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -10,3 +10,50 @@ export type ControlButtonProps = {
 export type IconProps = {
   size: number;
 };
+
+export type ProgressSliderProps = {
+  currentPosition: number;
+  onPositionChange: (value: number) => void;
+  totalDuration: number;
+  stepInterval: number;
+};
+
+export type YtdlResponse = {
+  formats: Track[];
+};
+
+type AudioQuality =
+  | 'AUDIO_QUALITY_LOW'
+  | 'AUDIO_QUALITY_MEDIUM'
+  | 'AUDIO_QUALITY_HIGH';
+
+export type Track = {
+  approxDurationMs: number;
+  audioQuality: AudioQuality;
+  quality: string;
+  qualityLabel: string;
+  hasAudio: boolean;
+  hasVideo: boolean;
+  url: string;
+};
+
+export type PlaybackStatus = {
+  isLoaded: true;
+  androidImplementation?: string;
+  uri: string;
+  progressUpdateIntervalMillis: number;
+  durationMillis: number;
+  positionMillis: number;
+  playableDurationMillis?: number;
+  seekMillisToleranceBefore?: number;
+  seekMillisToleranceAfter?: number;
+  shouldPlay: boolean;
+  isPlaying: boolean;
+  isBuffering: boolean;
+  rate: number;
+  shouldCorrectPitch: boolean;
+  volume: number;
+  isMuted: boolean;
+  isLooping: boolean;
+  didJustFinish: boolean;
+};
